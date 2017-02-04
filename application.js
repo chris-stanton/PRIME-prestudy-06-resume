0e0596cd6e65f14a7f85cd625e7754e1e3cb079d
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-04 12:43:29 -0600

    jQuery upload

--- application.js
+++ application.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function() {
 
-
+  $("header, p, img, h2, h3, h4, ul, li, div, footer").hide();    // hides all data
+    $("button").click(function(){
+        $("header, p, img, h2, h3, h4, ul, li, div, footer").show();    // reveals hidden data
+        $("button").hide();    // hides button
+    });
 });
